304a1df26f3df282c85047642605f2b6de1086d3
Joshua Wiens <joshuaw@easymetrics.com>
2018-01-25 08:32:35 -0600

    refactor(heros service): Move bootstrap code out of nest app

--- services/heros-service/src/app/app.module.ts
+++ services/heros-service/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { Module } from '@nestjs/common';
 
 import { SharedModule } from '../shared/shared.module';
 
-import { AppBootstrap } from './app.bootstrap';
-import { AppComponent } from './app.component';
-import { AppConfiguration } from './app.config';
-
 @Module({
   modules: [SharedModule, HerosModule],
   controllers: [],
-  components: [AppComponent, AppBootstrap, AppConfiguration],
-  exports: [AppComponent, AppBootstrap, AppConfiguration],
+  components: [],
+  exports: [],
 })
 export class AppModule {}
